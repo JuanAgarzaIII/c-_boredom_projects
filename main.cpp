b00bf6c312665e965e6ed7cfaae841b2177b1b27
JuanAgarzaIII <jgarza18@stu.ctcd.edu>
2019-11-08 01:50:54 +0000

    added loop validation from line 35-45.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ void inRange(char cStart, char cEnd, char lStart, char lEnd);
 int main() {
   const char capStart = 65, capEnd = 90;
   const char lowStart = 97, lowEnd = 122;
+  char bStart;
   
   inRange(capStart,capEnd,lowStart,lowEnd);
 
@@ -30,6 +31,18 @@ void inRange(char cStart, char cEnd, char lStart, char lEnd){
   cout << endl;
 
   cin >> choice;
+
+  while(choice < 1 | choice > 2){
+    cout <<"error,";
+    cout << "your startin position is the capital letter A, would you like to go backward"
+          "or foward " << endl;
+  cout << "1)forward" << endl;
+  cout << "2)backwards" << endl;
+  cout << endl;
+
+  cin >> choice;
+
+  }
   switch(choice){
 
     case 1: 
@@ -41,6 +54,8 @@ void inRange(char cStart, char cEnd, char lStart, char lEnd){
       cin >> places;
       break;
   }
+
+ while ()
 
 }
 
