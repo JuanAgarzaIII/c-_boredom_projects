1d8b34ab4d697672f34c45c6a39d705179e77043
JuanAgarzaIII <jgarza18@stu.ctcd.edu>
2019-11-08 01:32:57 +0000

    added basic function for range.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//function prototype
+void inRange(char cStart, char cEnd, char lStart, char lEnd);
+
+
+int main() {
+  const char capStart = 65, capEnd = 90;
+  const char lowStart = 97, lowEnd = 122;
+  
+  inRange(capStart,capEnd,lowStart,lowEnd);
+
+  cin.ignore();
+  cin.get();
+
+  return 0;
+}
+
+void inRange(char cStart, char cEnd, char lStart, char lEnd){
+
+  int choice, places;
+
+  cout << "your startin position is the capital letter A, would you like to go backward"
+          "or foward " << endl;
+  cout << "1)forward" << endl;
+  cout << "2)backwards" << endl;
+  cout << endl;
+
+  cin >> choice;
+  switch(choice){
+
+    case 1: 
+      cout << "how many places?" << endl;
+      cin >> places;
+      break;
+    case 2:
+      cout << "how many places?" << endl;
+      cin >> places;
+      break;
+  }
+
+}
+
